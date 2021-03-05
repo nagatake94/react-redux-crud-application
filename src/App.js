e17110fce84876a3537dbd305db01df4e96e7958
nagatak <76759306+nagatake94@users.noreply.github.com>
2021-03-05 17:31:34 +0900

    props復習

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 
 function App() {
+  const profiles = [
+    {name: "Kei", age: 24},
+    {name: "Messi", age: 32},
+    {name: "Makoto", age: 37},
+    {name: "Kazu", age: 53},
+    {name: "NoUser"}
+  ]
   return (
     <div>
-    <label htmlFor="bar">bar</label>
-    <input type="text" onChange={() => {console.log("Clicked")}}/>
+      {
+        profiles.map((profile, index)=>{
+          return <User name={profile.name} age={profile.age} key={index}/>
+        })
+      }
     </div>
   );
+}
+const User = (props) => {
+  return <div>Hi, I'm {props.name}, and {props.age} years old!</div>
+}
+
+User.defaultProps = {
+  age:1
 }
 
 export default App;
